37109da43e55db770a4a4779568fbbbeed6825bc
eulgen <ndengbrice@gmail.com>
2023-11-15 04:43:15 +0100

    robotfriends

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import App from "./App";
+import App from "./containers/App";
 import "./index.css";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
